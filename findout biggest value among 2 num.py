964263ead912a78866f56e02068c83bc932e62b1
soma vamsi <158448879+SOMAVAMSI@users.noreply.github.com>
2024-02-16 18:36:18 +0530

    python programming.

---
+++ findout biggest value among 2 num.py
@@ -0,0 +1,14 @@
+def find_biggest(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+# Example usage:
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+biggest = find_biggest(number1, number2)
+print("The biggest number is:", biggest)
+
+
